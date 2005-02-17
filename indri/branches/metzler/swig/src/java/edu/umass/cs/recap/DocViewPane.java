c00a5e26d7a7115d64e75565ff5f36ed2a227105
metzler <metzler@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-02-17 22:20:21 +0000

    RECAP update. Added the beginnings of HTMLDocument ranked list support and query annotation highlighting for the "explore" query mode.

--- indri/branches/metzler/swig/src/java/edu/umass/cs/recap/DocViewPane.java
+++ indri/branches/metzler/swig/src/java/edu/umass/cs/recap/DocViewPane.java
@@ -1,12 +1,26 @@
 package edu.umass.cs.recap;
 import java.awt.Dimension;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Style;
+import javax.swing.text.StyleContext;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+
+import edu.umass.cs.indri.QueryAnnotation;
+import edu.umass.cs.indri.QueryAnnotationNode;
+import edu.umass.cs.indri.ScoredExtentResult;
 
 /*
  * Created on Sep 28, 2004
@@ -19,7 +33,7 @@ import javax.swing.text.DefaultStyledDocument;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class DocViewPane extends JSplitPane {
+public class DocViewPane extends JSplitPane implements HyperlinkListener {
 
 	private RetrievalEngine retEngine = null;
 	
@@ -27,19 +41,22 @@ public class DocViewPane extends JSplitPane {
 	private JTextPane docTextPane = null;
 	private JScrollPane docPane = null;
 	
+	private HashMap curExploreResults = null;
+	
 	public DocViewPane( RetrievalEngine retEngine, Dimension screenSize ) {
 		super( JSplitPane.HORIZONTAL_SPLIT );
 		this.retEngine = retEngine;
+		this.curExploreResults = new HashMap();
 
 		docTextPane = new JTextPane();		
 		matchPane = new JTabbedPane( JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT );
 		docPane = new JScrollPane();
 		docPane.getViewport().setView( docTextPane );
-		docPane.setPreferredSize( new Dimension( (int)(0.5 * screenSize.width ),
-				                                 (int)(0.65 * screenSize.height ) ) );
 
 		setLeftComponent( docPane );
 		setRightComponent( matchPane );
+		
+		setResizeWeight( 0.5 );
 	}
 	
 	public void displayDoc( DocInfo info ) {
@@ -67,7 +84,50 @@ public class DocViewPane extends JSplitPane {
 		scrollPane.getViewport().setView( textPane );
 		matchPane.addTab( info.docName, scrollPane );
 	}
+	
+	// displays the results of an "Explore" query
+	public void displayExplorationResults( QueryAnnotation annotation, Vector viewableResults ) {
+		curExploreResults.clear();
+		ScoredExtentResult [] results = annotation.getResults();
+		QueryAnnotationNode root = annotation.getQueryTree();
+		Map matches = annotation.getAnnotations();
 		
+		for( int i = 0; i < results.length; i++ ) {
+			curExploreResults.put( new Integer( i ), retEngine.getAnnotatedDocument( results[i].document, annotation ) );
+		}
+		
+		addResultTab( results, viewableResults );
+	}
+	
+	private void addResultTab( ScoredExtentResult [] results, Vector viewableResults ) {
+		JScrollPane resultPane = new JScrollPane();
+		JTextPane textPane = new JTextPane();
+		textPane.setContentType("text/html");
+		HTMLEditorKit kit = new HTMLEditorKit();
+		HTMLDocument doc = (HTMLDocument)kit.createDefaultDocument();
+		setResults( doc, results, viewableResults );
+		textPane.setDocument( doc );
+		textPane.setEditable( false );
+		textPane.addHyperlinkListener( this ); // listen for hyperlink clicks
+		resultPane.getViewport().setView( textPane );
+		matchPane.addTab( "Results", resultPane );
+	}
+
+	private void setResults( HTMLDocument doc, ScoredExtentResult [] results, Vector viewableResults ) {
+		HTMLEditorKit htmlKit = new HTMLEditorKit();
+		
+		for( int i = 0; i < results.length; i++ ) {
+			ScoredDocInfo info = (ScoredDocInfo)viewableResults.elementAt(i);
+			String url = info.docName + ":" + i;
+			String title = "<h2>" + (i+1) + ". <a href=\"" + url + "\">" + info.docName + "</a></h2>";
+					
+			try {
+				htmlKit.insertHTML( doc, doc.getLength(), title, 0, 0, null );
+			}
+			catch( Exception e ) { e.printStackTrace(); }
+		}		
+	}
+	
 	public JTabbedPane getMatchPane() {
 		return matchPane;
 	}
@@ -88,5 +148,16 @@ public class DocViewPane extends JSplitPane {
 	
 	public void clearTabs() {
 		matchPane.removeAll();
+	}
+
+	// process a hyperlink click
+	public void hyperlinkUpdate(HyperlinkEvent e) {
+		if( e.getEventType() == HyperlinkEvent.EventType.ACTIVATED ) {
+			StringTokenizer tok = new StringTokenizer( e.getDescription(), ":" );
+			String docName = tok.nextToken();
+			int rank = Integer.parseInt( tok.nextToken() );			
+			docTextPane.setDocument( (DefaultStyledDocument)curExploreResults.get( new Integer( rank ) ) );			
+			setDividerLocation( 0.5 );
+		}
 	}
 }
---
+++ indri/branches/metzler/swig/src/java/edu/umass/cs/recap/Pair.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Feb 17, 2005
+ *
+ */
+package edu.umass.cs.recap;
+
+/**
+ * @author Don Metzler
+ *
+ */
+public class Pair {
+
+	public Object left;
+	public Object right;
+	
+	public Pair( Object a, Object b ) {
+		this.left = a;
+		this.right = b;
+	}
+	
+}
