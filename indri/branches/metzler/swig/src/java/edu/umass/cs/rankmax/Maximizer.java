4b1aec29e1021007437a90f13be5f042e13ad967
metzler <metzler@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-04-21 12:51:59 +0000

    RankMax update. Initial implementations of bracket() and lineSearch() in Maximizer.java are complete. CoordinateAscentMaximizer.java was updated to use the new methods.

--- indri/branches/metzler/swig/src/java/edu/umass/cs/rankmax/Maximizer.java
+++ indri/branches/metzler/swig/src/java/edu/umass/cs/rankmax/Maximizer.java
@@ -46,37 +46,99 @@ abstract public class Maximizer {
 	}
 
 	// brackets a maximum around the current parameter setting in the given direction
-	// returns null if no bracket could be found
-	// TODO: implement this
-	protected double [] bracket( Parameters direction, double initialStepSize, double minAlpha, double maxAlpha ) {		
-		double [] bracket = new double[2];
-		double alpha, stepSize;
+	// this is pretty heuristic and not guaranteed to find a bracket even if one does
+	// exist. however, it should find a majority of the 'well-behaved' brackets.
+	protected Bracket bracket( Parameters direction, double initialStepSize, double minAlpha, double maxAlpha ) {		
+		Bracket bracket = new Bracket( Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
+									   0.0, 0.0,
+									   Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY );
+		double alpha, stepSize, lastVal;
+		boolean increase, decrease;
 		
 		// value of current parameter setting
 		// in order to find a bracket we need to find two points a and b such that:
 		// a < param < b AND f(a) < f(param) > f(b)
 		double val = eval( param );
+		bracket.fb = val;
 		
 		// search in the positive direction
 		alpha = initialStepSize;
 		stepSize = initialStepSize;
+		increase = false;
+		decrease = false;
+		lastVal = val;
 		while( alpha <= maxAlpha ) {
-			double fb = eval( param.add( direction, 1.0, stepSize ) );
+			double newVal = eval( param.add( direction, 1.0, alpha ) );
+			
+			if( newVal > lastVal ) { increase = true; }
+			if( newVal < lastVal ) { decrease = true; }
+			
+			//System.out.println( "[RIGHT] alpha: " + alpha + ", lastVal: " + lastVal + ", newVal: " + newVal );
+			
+			if( !increase && decrease ) { // right end of bracket
+				bracket.c = alpha;
+				bracket.fc = newVal;
+				break;
+			}
+			else if( increase && decrease ) { // entire bracket
+				bracket.a = 0;
+				bracket.fa = val;
+				bracket.b = alpha - stepSize;
+				bracket.fb = lastVal;
+				bracket.c = alpha;
+				bracket.fc = newVal;
+				return bracket;
+			}
+			
 			stepSize *= 2; // double the step size each time
 			if( alpha != maxAlpha && alpha + stepSize > maxAlpha )
 				alpha = maxAlpha;
 			alpha += stepSize;
+			lastVal = newVal;
+		}
+		if( !decrease ) {
+			bracket.c = maxAlpha;
+			bracket.fc = lastVal;
 		}
 		
 		// search in the negative direction
 		alpha = -initialStepSize;
 		stepSize = initialStepSize;
+		increase = false;
+		decrease = false;
+		lastVal = val;
 		while( alpha >= minAlpha ) {
-			double fa = eval( param.add( direction, 1.0, stepSize ) );
+			double newVal = eval( param.add( direction, 1.0, alpha ) );
+			
+			if( newVal > lastVal ) { increase = true; }
+			if( newVal < lastVal ) { decrease = true; }
+
+			//System.out.println( "[LEFT] alpha: " + alpha + ", lastVal: " + lastVal + ", newVal: " + newVal );
+			
+			if( !increase && decrease ) { // left end of bracket
+				bracket.a = alpha;
+				bracket.fa = newVal;
+				break;
+			}
+			else if( increase && decrease ) { // entire bracket
+				bracket.a = alpha;
+				bracket.fa = newVal;
+				bracket.b = alpha - stepSize;
+				bracket.fb = lastVal;
+				bracket.c = 0.0;
+				bracket.fc = val;
+				return bracket;
+			}
+			
 			stepSize *= 2; // double the step size each time
 			if( alpha != minAlpha && alpha - stepSize < minAlpha )
-				alpha = maxAlpha;
+				alpha = minAlpha;
 			alpha -= stepSize;
+			lastVal = newVal;
+		}
+		if( !decrease ) {
+			bracket.a = minAlpha;
+			bracket.fa = lastVal;
 		}
 		
 		return bracket;
@@ -85,7 +147,7 @@ abstract public class Maximizer {
 	// performs the line search max_{\alpha} f(param + \alpha * direction)
 	// implemented as golden section search based on implementation from Numerical Recipes in C
 	// TODO: implement Brent's method?
-	protected double lineSearch( Parameters direction, double minAlpha, double maxAlpha ) {
+	protected double lineSearch( Parameters direction, Bracket bracket ) {
 		// points we keep track of
 		Parameters x0 = null;
 		Parameters x1 = null;
@@ -96,22 +158,24 @@ abstract public class Maximizer {
 		double f1 = 0.0;
 		double f2 = 0.0;
 		
-		x0 = param.add( direction, 1.0, minAlpha ); // param + minAlpha
-		x3 = param.add( direction, 1.0, maxAlpha ); // param + maxAlpha
+		x0 = param.add( direction, 1.0, bracket.a ); // param + minAlpha
+		x3 = param.add( direction, 1.0, bracket.c); // param + maxAlpha
 		
-		if( Math.abs( maxAlpha - 0 ) > Math.abs( 0 - minAlpha ) ) {
+		if( Math.abs( bracket.c - bracket.b ) > Math.abs( bracket.b - bracket.a ) ) {
 			x1 = param;
-			x2 = param.add( direction, 1.0, C*( maxAlpha - 0 ) );
+			x2 = param.add( direction, 1.0, C*( bracket.c - bracket.b ) );
 		}
 		else {
 			x2 = param;
-			x1 = param.add( direction, 1.0, -C*( 0 - minAlpha ) );
+			x1 = param.add( direction, 1.0, -C*( bracket.a - bracket.a ) );
 		}
 		
 		f1 = eval( x1 );
 		f2 = eval( x2 );
 
-		while( Math.abs( x3 - x0 ) > TOL*( Math.abs( x1 ) + Math.abs( x2 ) ) ) {
+		//while( Math.abs( x3 - x0 ) > TOL*( Math.abs( x1 ) + Math.abs( x2 ) ) ) {
+		// TODO: implement better stopping criteria
+		for( int iter = 0; iter < 5; iter++ ) {
 			if( f2 > f1 ) {
 				x0 = x1;
 				x1 = x2;
@@ -135,12 +199,45 @@ abstract public class Maximizer {
 		else {
 			param = x2;			
 			return f2;
-		}
-		
-		return 0.0;
+		}		
 	}
 	
 	public void setVerbose( boolean b ) {
 		verbose = b;
+	}
+	
+	public class Bracket {
+		public double a, fa;
+		public double b, fb;
+		public double c, fc;
+		
+		public Bracket() {
+			this.a = 0.0;
+			this.fa = 0.0;
+			this.b = 0.0;
+			this.fb = 0.0;
+			this.c = 0.0;
+			this.fc = 0.0;			
+		}
+		
+		public Bracket( double a, double fa, double b, double fb, double c, double fc ) {
+			this.a = a;
+			this.fa = a;
+			this.b = b;
+			this.fb = b;
+			this.c = c;
+			this.fc = c;
+		}
+		
+		// returns whether or not this is actually a bracket or not
+		public boolean isBracket() {
+			if( a < b && b < c && fa < fb && fb > fc )
+				return true;
+			return false;
+		}
+		
+		public String toString() {
+			return "[ " + a + " (" + fa + "), " + b + " (" + fb + "), " + c + " (" + fc + ") ]";
+		}
 	}
 }
