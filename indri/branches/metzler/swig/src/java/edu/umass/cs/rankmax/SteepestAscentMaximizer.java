2b580e7b67f6976225e768d9b781c92a8e47267d
metzler <metzler@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-04-22 13:14:32 +0000

    RankMax update. Initial implementation of SteepestAscentMaximizer.java and fixes to CoordinateAscentMaximizer.java. Maximizer.java now includes a method for estimating finite difference approximations of the evaluation function gradient.

--- indri/branches/metzler/swig/src/java/edu/umass/cs/rankmax/SteepestAscentMaximizer.java
+++ indri/branches/metzler/swig/src/java/edu/umass/cs/rankmax/SteepestAscentMaximizer.java
@@ -30,6 +30,8 @@ public class SteepestAscentMaximizer extends Maximizer {
 	
 	public void maximize() {
 		double curVal = eval( param );
+		Parameters gradient = null;
+		Bracket bracket = null;
 
 		for( int iter = 0; iter < MAX_ITERS; iter++ ) {
 			if( verbose ) {
@@ -38,110 +40,37 @@ public class SteepestAscentMaximizer extends Maximizer {
 				System.out.println( "FXN VALUE = " + curVal );
 			}
 
-			// perform one optimization step
-			for( int i = 0; i < param.size(); i++ ) {
-				//curVal = lineSearch( curVal, i );			
-				if( onSimplex )
-					param.simplexNormalize();
+			// find direction of steepest ascent
+			gradient = getFiniteDifferenceGradient( -25.0, 25.0 );
+			System.out.println( "GRADIENT: " + gradient );
+			
+			// try to bracket a maxima in the direction of the gradient
+			bracket = bracket( gradient, 10E-16, -25.0, 25.0 );		
+			System.out.println( bracket );
+			
+			// take a step in an increasing direction
+			if( bracket.isBracket() )
+				curVal = lineSearch( gradient, bracket ); // do a line search to find optimal step size
+			else {
+				if( bracket.fa > curVal ) {
+					param = param.add( gradient, 1.0, bracket.a );
+					curVal = bracket.fa;
+				}
+				else if( bracket.fb > curVal ) {
+					param = param.add( gradient, 1.0, bracket.b );
+					curVal = bracket.fb;
+				}
+				else if( bracket.fc > curVal ) {
+					param = param.add( gradient, 1.0, bracket.c );
+					curVal = bracket.fc;
+				}
 			}
+			if( onSimplex )
+				param.simplexNormalize();
 		}
 		
 		if( verbose )
 			System.out.println( "Total function evaluations = " + fxnEvaluations );
 	}
 	
-/*	protected double lineSearch( double curVal, int coordinate ) {
-		double newVal = curVal;
-		
-		double originalVal = param.getParam( coordinate );
-		double pointA = 0.0;
-		double pointB = 0.0;
-		
-		// set up bracket
-		// TODO: automatically find bracket
-		if( onSimplex ) {
-			pointA = 0.25*originalVal;
-			pointB = 4.0*originalVal;
-		}
-		else {
-			pointA = originalVal - BRACKET_WIDTH;
-			pointB = originalVal + BRACKET_WIDTH;
-		}
-			
-		param.setParam( coordinate, pointA );
-		double fa = eval( param );
-		param.setParam( coordinate, pointB );
-		double fb = eval( param );
-		param.setParam( coordinate, originalVal );
-
-		if( fa >= curVal && fa > fb ) { // non-bracket
-			param.setParam( coordinate, pointA );
-			newVal = fa;
-		}
-		else if( fb >= curVal && fb > fa ) { // non-bracket
-			param.setParam( coordinate, pointB );
-			newVal = fb;
-		}
-		else { // bracket [ a, curVal, c ]
-			//System.out.println( "originalVal: " + originalVal + ", coordinate: " + coordinate + ", a: " + fa + ", curVal:" + curVal + ", b: " + fb );
-			newVal = goldenSectionSearch( coordinate, pointA, fa, originalVal, curVal, pointB, fb );
-		}
-		
-		return newVal;
-	}
-		
-	// golden section one dimension search
-	protected double goldenSectionSearch( int coordinate,
-										  double a, double fa,
-										  double b, double fb,
-										  double c, double fc ) {
-		double val = 0.0;
-		double d = 0.0;
-		double R = ( 3.0 - Math.sqrt( 5.0 ) ) / 2.0;
-
-		for( int iter = 0; iter < 50; iter++ ) {
-			// get new point to evaluate
-			if( c - b > b - a )
-				d = b + R*( c - b );
-			else
-				d = b - R*( b - a );
-		
-			param.setParam( coordinate, d );
-			double fd = eval( param );
-
-			//System.out.println( "original bracket: [ " + a + "(" + fa + "), " + b + "(" + fb + "), " + c + "(" + fc + ") ]" );
-		
-			// TODO: simplify this logic once i make sure it's correct
-			if( d > b ) {
-				if( fb >= fd ) {
-					c = d;
-					fc = fd;
-				}
-				else {
-					a = b;
-					fa = fb;
-					b = d;
-					fb = fd;
-				}
-			}
-			else {
-				if( fb >= fd ) {
-					a = d;
-					fa = fd;
-				}
-				else {
-					c = b;
-					fc = fb;
-					b = d;
-					fb = fd;
-				}
-			}
-		}			
-		
-		//System.out.println( "new bracket: [ " + a + "(" + fa + "), " + b + "(" + fb + "), " + c + "(" + fc + ") ]" );
-		param.setParam( coordinate, b );
-		
-		return fb;
-	}*/
-
 }
