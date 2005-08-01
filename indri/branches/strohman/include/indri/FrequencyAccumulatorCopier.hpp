d87200f58ebc2cfac38b39b9801217de3e7cc584
strohman <strohman@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-08-01 15:52:28 +0000

    Added TermFrequencyAccumulator headers

---
+++ indri/branches/strohman/include/indri/FrequencyAccumulatorCopier.hpp
@@ -0,0 +1,78 @@
+/*==========================================================================
+ * Copyright (c) 2004 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+ */
+
+//
+// FrequencyAccumulatorCopier
+//
+// 29 July 2005 -- tds
+//
+
+#ifndef INDRI_FREQUENCYACCUMULATORCOPIER_HPP
+#define INDRI_FREQUENCYACCUMULATORCOPIER_HPP
+
+namespace indri
+{
+  namespace lang 
+  {
+    class FrequencyAccumulatorCopier : public indri::lang::Copier {
+    private:
+      std::vector<indri::lang::Node*> _nodes;
+
+    public:
+      ~WeightFoldingCopier() {
+        indri::utility::delete_vector_contents( _nodes );
+      }
+
+      indri::lang::Node* defaultAfter( indri::lang::Node* old, indri::lang::Node* newNode ) {
+        _nodes.push_back( newNode );
+        return newNode;
+      }
+
+      indri::lang::Node* after( indri::lang::ScoreAccumulatorNode* oldAccumulatorNode, indri::lang::ScoreAccumulatorNode* newAccumulatorNode ) {
+        // if we have a WeightNode child, and it has children that are only TermFrequencyNodes, we should swap in the TermFrequencyAccumulatorNode
+        indri::lang::WeightNode* child = dynamic_cast<indri::lang::WeightNode*>( newAccumulatorNode->getChild() );
+        
+        if( !child ) {
+          // child isn't a WeightNode, so we can't replace it
+          _nodes.push_back( newNode );
+          return newAccumulatorNode;
+        }
+
+        std::vector< std::pair<double, indri::lang::ScoredExtentNode*> >& children = child->getChildren();
+
+        for( int i=0; i<children.size(); i++ ) {
+          indri::lang::TermFrequencyScorerNode* tfs = dynamic_cast<indri::lang::TermFrequencyScorerNode*>( children[i] );
+
+          if( !tfs ) {
+            // found a node that's not a term frequency node, so we can't continue;
+            _nodes.push_back( newNode );
+            return newAccumulatorNode;
+          }
+        }
+
+        // everything checks out, so make a TermFrequencyAccumulatorNode instead
+
+        newerWeightNode->setNodeName( newWeightNode->nodeName() );
+
+
+        indri::lang::TermFrequencyAccumulatorNode* accumulator = new indri::lang::TermFrequencyAccumulatorNode( children );
+        accumulator->setNodeName( newAccumulatorNode->nodeName() );
+        delete newAccumulatorNode;
+        _nodes.pop_back( accumulator );
+
+        return accumulator;
+      }
+    };
+  }
+}
+
+#endif // INDRI_FREQUENCYACCUMULATORCOPIER_HPP
+
---
+++ indri/branches/strohman/include/indri/TermFrequencyAccumulator.hpp
@@ -0,0 +1,107 @@
+/*==========================================================================
+ * Copyright (c) 2004 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+ */
+
+//
+// TermFrequencyAccumulator.hpp
+//
+// Processes simple title queries (#combine or #weight
+// around only simple terms or stopwords)  The integrated
+// approach is a bit more efficient, and makes it possible
+// to cut out query processing in the event that topdocs
+// have all the necessary information.
+//
+
+#ifndef INDRI_TERMFREQUENCYACCUMULATOR_HPP
+#define INDRI_TERMFREQUENCYACCUMULATOR_HPP
+
+#include "indri/EvaluatorNode.hpp"
+#include "indri/greedy_vector"
+#include "indri/TermScoreFunction.hpp"
+#include "indri/BeliefNode.hpp"
+#include "indri/TermFrequencyBeliefNode.hpp"
+#include <queue>
+#include <algorithm>
+
+namespace indri {
+  namespace infnet {
+    class TermFrequencyAccumulator : public EvaluatorNode {
+    private:
+      struct child_type {
+        struct maxscore_less {
+        public:
+          bool operator () ( const child_type& one, const child_type& two ) const {
+            // think of these two elements as the only two elements in the
+            // #wand.  What is the threshold of each ordering?  Sort by
+            // the lowest threshold.
+
+        return (one.backgroundWeightedScore) > 
+               (two.backgroundWeightedScore);
+          }
+        };
+
+        BeliefNode* node;
+        double weight;
+        double maximumWeightedScore;
+        double backgroundWeightedScore;
+      };
+
+      void _findPotentialMatches( indri::utility::greedy_vector< int >& matches,
+                                  const indri::utility::greedy_vector<indri::index::DocListIterator::TopDocument>& all,
+                                  int nodeCount );
+      void _buildSingleTermMatches( indri::utility::greedy_vector< int >& matches,
+                                    const indri::utility::greedy_vector<indri::index::DocListIterator::TopDocument>& all,
+                                    indri::query::TermScoreFunction& function,
+                                    int maximumDocumentLength,
+                                    int k );
+      void _scorePotentialMatches( const indri::utility::greedy_vector< int >& matches,
+                                   const indri::utility::greedy_vector< indri::index::DocListIterator::TopDocument >& all,
+                                   const indri::utility::greedy_vector< indri::query::TermScoreFunction* >& functions,
+                                   const indri::utility::greedy_vector< double >& weights );
+      void _buildCandidatesList( const indri::utility::greedy_vector<indri::index::DocListIterator::TopDocument>& all );
+      bool _topdocsScoresAreSufficient( const indri::utility::greedy_vector< indri::infnet::TermFrequencyBeliefNode* >& nodes,
+                                        const indri::utility::greedy_vector< double >& weights );
+
+      void _precomputeQuery();
+
+      double _computeMaxScore( unsigned int start );
+      void _computeQuorum();
+
+      std::priority_queue<indri::api::ScoredExtentResult> _scores;
+      std::vector<child_type> _children;
+      std::vector<int> _candidates;
+      bool _complex;
+      int _resultsRequested;
+      int _candidatesIndex;
+      int _quorumIndex;
+
+    public:
+      TermFrequencyAccumulator() {}
+
+      bool isComplex();
+    
+      void addChild( double weight, indri::infnet::BeliefNode* node );
+      void indexChanged( indri::index::Index& index );
+      void setThreshold( double threshold );
+      int nextCandidateDocument();
+      void evaluate( int documentID, int documentLength );
+      const std::string& getName() const;
+    };
+  }
+}
+
+#endif // INDRI_TERMFREQUENCYACCUMULATOR_HPP
+
+
+
+
+
+//
+
