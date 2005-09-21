f8d1c7e0bffbf2a4022968c34ef86be9729078bb
strohman <strohman@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-09-21 18:03:06 +0000

    Copying the trunk into strohman branch

---
+++ indri/branches/strohman/trunk/src/FieldGreaterNode.cpp
@@ -0,0 +1,64 @@
+/*==========================================================================
+ * Copyright (c) 2004 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+ */
+
+
+//
+// FieldGreaterNode
+//
+// 28 July 2004 -- tds
+//
+
+#include "indri/FieldGreaterNode.hpp"
+#include "indri/Annotator.hpp"
+#include "indri/FieldIteratorNode.hpp"
+
+indri::infnet::FieldGreaterNode::FieldGreaterNode( const std::string& name, FieldIteratorNode* iterator, INT64 constant ) {
+  _name = name;
+  _field = iterator;
+  _constant = constant;
+}
+
+void indri::infnet::FieldGreaterNode::prepare( int documentID ) {
+  _extents.clear();
+
+  if( !_field )
+    return;
+
+  const indri::utility::greedy_vector<INT64>& numbers = _field->numbers();
+  const indri::utility::greedy_vector<indri::index::Extent>& extents = _field->extents();
+
+  for( unsigned int i=0; i<numbers.size(); i++ ) {
+    if( numbers[i] > _constant ) {
+      _extents.push_back( extents[i] );
+    }
+  }
+}
+
+indri::utility::greedy_vector<indri::index::Extent>& indri::infnet::FieldGreaterNode::extents() {
+  return _extents;
+}
+
+int indri::infnet::FieldGreaterNode::nextCandidateDocument() {
+  return _field->nextCandidateDocument();
+}
+
+const std::string& indri::infnet::FieldGreaterNode::getName() const {
+  return _name;
+}
+
+void indri::infnet::FieldGreaterNode::annotate( indri::infnet::Annotator& annotator, int documentID, int begin, int end ) {
+  annotator.addMatches( _extents, this, documentID, begin, end );
+}
+
+void indri::infnet::FieldGreaterNode::indexChanged( indri::index::Index& index ) {
+  // do nothing
+}
+
