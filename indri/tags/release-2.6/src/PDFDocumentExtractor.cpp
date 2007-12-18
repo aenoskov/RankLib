3e6f579386f6170b98e6704620b669de809115d1
dfisher <dfisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2007-12-18 16:09:05 +0000

    made a copy

---
+++ indri/tags/release-2.6/src/PDFDocumentExtractor.cpp
@@ -0,0 +1,114 @@
+/*==========================================================================
+ * Copyright (c) 2003-2004 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+*/
+
+
+//
+// PDFDocumentExtractor
+//
+// 25 June 2004 -- tds
+//
+
+#include "indri/PDFDocumentExtractor.hpp"
+#include "indri/Buffer.hpp"
+
+#include "GString.h"
+#include "TextOutputDev.h"
+#include "PDFDoc.h"
+
+#include "Object.h"
+#include "Stream.h"
+#include "Array.h"
+#include "Dict.h"
+#include "XRef.h"
+#include "Page.h"
+#include "CharTypes.h"
+#include "GlobalParams.h"
+
+static void buffer_write( void* stream, char* text, int len ) {
+  indri::utility::Buffer* buffer = (indri::utility::Buffer*) stream;
+
+  if( buffer->position() ) {
+    buffer->unwrite(1);
+  }
+
+  memcpy( buffer->write(len), text, len );
+  if( text[len-1] != 0 )
+    *buffer->write(1) = 0;
+}
+
+indri::parse::PDFDocumentExtractor::PDFDocumentExtractor() {
+  globalParams = new GlobalParams(0);
+}
+
+indri::parse::PDFDocumentExtractor::~PDFDocumentExtractor() {
+  delete globalParams;
+  globalParams = 0;
+}
+
+void indri::parse::PDFDocumentExtractor::open( const std::string& filename ) {
+  _documentTextBuffer.clear();
+  _documentPath = filename;
+}
+
+void indri::parse::PDFDocumentExtractor::close() {
+  _documentPath = "";
+}
+
+indri::parse::UnparsedDocument* indri::parse::PDFDocumentExtractor::nextDocument() {
+  if( !_documentPath.length() )
+    return 0;
+
+  PDFDoc* doc = 0;
+  TextOutputDev* textOut = 0;
+  GString* gfilename = new GString(_documentPath.c_str());
+  doc = new PDFDoc( gfilename );
+  // if the doc is not ok, or ok to copy, it
+  // will be a document of length 0.
+  if( doc->isOk() && doc->okToCopy() ) {
+    void* stream = &_documentTextBuffer;
+    textOut = new TextOutputDev( buffer_write, stream, gFalse, gFalse);
+  
+    if ( textOut->isOk() ) {
+      int firstPage = 1;
+      int lastPage = doc->getNumPages();
+
+      doc->displayPages(textOut, firstPage, lastPage, 72, 72, 0, gTrue, gFalse);
+    }
+  }
+  
+
+  delete textOut;
+  delete doc;
+
+  _unparsedDocument.textLength = _documentTextBuffer.position();
+  _unparsedDocument.contentLength = _unparsedDocument.textLength ? _documentTextBuffer.position() - 1 : 0 ; // no null 0 if text is empty.
+  char* docnoPoint = _documentTextBuffer.write( _documentPath.length()+1 );
+  strcpy( docnoPoint, _documentPath.c_str() );
+  _unparsedDocument.text = _documentTextBuffer.front();
+  _unparsedDocument.content = _documentTextBuffer.front();
+  _unparsedDocument.metadata.clear();
+
+  indri::parse::MetadataPair pair;
+
+  pair.key = "path";
+  pair.value = docnoPoint;
+  pair.valueLength = _documentPath.length()+1;
+  _unparsedDocument.metadata.push_back( pair );
+
+  pair.key = "docno";
+  pair.value = docnoPoint;
+  pair.valueLength = _documentPath.length()+1;
+  _unparsedDocument.metadata.push_back( pair );
+
+  _documentPath = "";
+
+  return &_unparsedDocument;
+}
